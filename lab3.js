e7363f21511dc898b9f744ce4a3d80cd5722ede1
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-05-26 10:52:02 -0400

    lab3 part 2

--- lab3.js
+++ lab3.js
@@ -1,6 +1,8 @@
 "use strict";
 // create variable called map that is any
 var map;
+// initialize your empty array with an empty array - if you don't, it will be undefined
+var coolLocations = [];
 // We are defining coordinates AS a LatLng type (aka with interface)
 // Letting coordinates be typed to LatLng interface. That means that it is using LatLng as a template for how it will be typed. Can't declare any other properties.
 var coordinates = {
@@ -11,6 +13,25 @@ var initMapConfig = {
     center: coordinates,
     zoom: 8
 };
+
+
+// PART 2
+$.ajax({
+    url: 'locations.json',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        //data is an aray of objects in this context
+        // push into your cool locations array
+        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+            var cl = data_1[_i];
+            // have an array of objects, and assign markers to each of those objects - use the address property
+            coolLocations.push(cl);
+        }
+    }
+});
+
+
 function initMap() {
     // center the map around toronto
     map = new google.maps.Map(document.getElementById("map"), initMapConfig);
--- lab3.ts
+++ lab3.ts
@@ -1,5 +1,7 @@
 // create variable called map that is any
 let map: any;
+// initialize your empty array with an empty array - if you don't, it will be undefined
+let coolLocations : any[] = [];
 
 // Define an interface with two properties
 // Purposes of interfaces is to define shape to your code - this won't be seen in your js file
@@ -30,3 +32,19 @@ function initMap() {
     // center the map around toronto
     map = new google.maps.Map(document.getElementById("map"), initMapConfig)
 }
+
+// PART 2
+$.ajax({
+    url: 'locations.json',
+    dataType: 'json',
+    success: function(data) {
+        console.log(data);
+
+        //data is an aray of objects in this context
+        // push into your cool locations array
+        for(let cl of data) {
+            // have an array of objects, and assign markers to each of those objects - use the address property
+            coolLocations.push(cl);
+        }
+    }
+})
