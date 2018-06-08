da7635b6a1f5e206efe48ccec2be1471306d3344
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-06-08 13:05:15 -0400

    Person model

---
+++ src/app/person.ts
@@ -0,0 +1,23 @@
+// Define the properties of the class with their data types
+
+export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+
+    // AGE IS DIFFERENCE BETWEEN TODAY'S DATE AND THE CURRENT YEAR
+    //     getAge():number {
+    //     let today: Date = new Date();
+    //     let age: number = today.getFullYear() - this.dateOfBirth.getFullYear();
+
+    //     if(this.dateOfBirth.getDate() > today.getDate() 
+    //     && this.dateOfBirth.getMonth() > today.getMonth()) {
+    //         age--;
+    //     }
+
+    //     return age;
+    // }
+}
+
+
